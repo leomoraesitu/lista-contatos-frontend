38ccd8a29ec02e10539918ffd651d06e38c9d10e
leomoraesitu <leomoraesitu@hotmail.com>
2023-11-21 18:00:05 -0300

    interface

---
+++ src/app/Model/contato.ts
@@ -0,0 +1,10 @@
+export interface Contato {
+    id: number,
+    name: string,
+    email: string,
+    telefone: number,
+    endereço: string,
+    cidade: string,
+    cep: number,
+    estado: string
+}
